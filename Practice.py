17fb2874d9900585b850f14afac9d5604bc06847
hritikgiri55-boop <hritikgiri55@gmail.com>
2026-07-16 05:10:45 +0000

    created spark session

--- Practice.py
+++ Practice.py
@@ -1,3 +1,9 @@
 # Databricks notebook source
+# /// script
+# [tool.databricks.environment]
+# environment_version = "5"
+# ///
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
+
+spark = SparkSession.builder.appName("Test").getOrCreate()
